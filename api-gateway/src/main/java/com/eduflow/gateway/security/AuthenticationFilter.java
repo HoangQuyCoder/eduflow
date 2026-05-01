6d03403ad0b5ec94632880970ab9d1ceff95a3a6
HoangQuyCoder <hoangquyle11@gmail.com>
2026-05-01 17:16:06 +0700

    fix check healthy eureka server

--- api-gateway/src/main/java/com/eduflow/gateway/security/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/eduflow/gateway/security/AuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.eduflow.gateway.security;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -8,9 +9,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+    
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationFilter.class);
     
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
--- api-gateway/src/main/java/com/eduflow/gateway/security/JwtTokenProvider.java
+++ api-gateway/src/main/java/com/eduflow/gateway/security/JwtTokenProvider.java
@@ -7,39 +7,38 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-    
+
     @Value("${jwt.secret}")
     private String secret;
-    
+
     @Value("${jwt.expiration}")
     private long expiration;
-    
+
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(secret.getBytes());
     }
-    
+
     public Claims getClaimsFromToken(String token) {
-        return Jwts.parserBuilder()
-            .setSigningKey(getSigningKey())
-            .build()
-            .parseClaimsJws(token)
-            .getBody();
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
-    
+
     public String getUserIdFromToken(String token) {
         return getClaimsFromToken(token).getSubject();
     }
-    
+
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token);
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
+                    .build()
+                    .parseSignedClaims(token);
             return true;
         } catch (Exception e) {
             return false;
